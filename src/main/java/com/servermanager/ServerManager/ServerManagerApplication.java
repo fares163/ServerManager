1fd144bdf24f6ee4e5fe0b1183b5c7aca92fd5dd
fares163 <faresbenagia@yahoo.fr>
2023-12-08 15:14:29 +0100

    adding to git

---
+++ src/main/java/com/servermanager/ServerManager/ServerManagerApplication.java
@@ -0,0 +1,13 @@
+package com.servermanager.ServerManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/servermanager/ServerManager/ServerManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.servermanager.ServerManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
